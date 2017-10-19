151b40f5ca52e7c5b54b67bd2c659fd0ef392ba1
Zonche <daniellLopz@hotmail.com>
2017-10-18 22:49:36 -0700

    Add files via upload

--- ejemploInventario/ejemploInventario/conexionSQL.cs
+++ ejemploInventario/ejemploInventario/conexionSQL.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace ejemploInventario
 {
     class conexionSQL
     {
-        SqlConnection myConnection = new SqlConnection("server=localhost; database=prueba1; user id=sa; password=sasa;");
+        SqlConnection myConnection = new SqlConnection("Data Source=localhost;Initial Catalog=prueba1;Integrated Security=True");
         string user, pass;
-        public void agregarUsuarios(string name_user, string password)
+        public void agregarUsuarios(string[] datos_de_usuario)
         {
-
+            myConnection.Open();
+            string query = string.Format("INSERT INTO informacion (nick,contraseña,nombre,apellidos,fecha_nacimiento,ciudad) VALUES('{0}','{1}','{2}','{3}','{4}')",
+                datos_de_usuario[0],
+                datos_de_usuario[1],
+                datos_de_usuario[2],
+                datos_de_usuario[3],
+                datos_de_usuario[4]);
         }
 
         public bool Login(string name_user, string password)
@@ -33,6 +40,19 @@ namespace ejemploInventario
                 return true;
             else
                 return false;
+        }
+
+        public DataTable tabla(string sql)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            da.SelectCommand = new SqlCommand(sql, myConnection);
+
+            da.Fill(ds);
+
+            dt = ds.Tables[0];
+            return dt;
         }
     }
 }
